2bef2a51e75327d44aa42f9c930dbe862d7723ac
Emil Dimitrov <edimitrov1@gmail.com>
2018-08-07 15:46:41 +0300

    Can play a single round

--- game.js
+++ game.js
@@ -19,6 +19,26 @@ function randomNumber() {
   return Math.floor(Math.random() * 3);
 }
 
+function playRound(playerSelection, computerSelection) {
+  const player = playerSelection.toLowerCase();
+  const comp = computerSelection.toLowerCase();
+  let result;
 
+  if ((player === 'rock' && comp === 'scissors') ||
+      (player === 'scissors' && comp === 'paper') ||
+      (player === 'paper' && comp === 'rock')) {
+        result = `You win! ${playerSelection} beats ${computerSelection}!`;
+  } else if ((player === 'rock' && comp === 'rock') ||
+             (player === 'scissors' && comp === 'scissors') ||
+             (player === 'paper' && comp === 'paper')) {
+               result = `It's a tie! ${playerSelection} equals ${computerSelection}.`;
+  } else {
+    result = `You lose! ${computerSelection} beats ${playerSelection}!`;
+  }
 
+  return result;
+}
 
+const player = 'Rock';
+const comp = computerPlay();
+console.log(playRound(player, comp));
